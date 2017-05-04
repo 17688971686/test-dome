503166b221a8765be529028207248bde52d48f69
tzg <tang_guo_168@163.com>
2017-05-04 15:11:39 +0800

    添加忽略

--- szcshl-web/src/main/java/cs/ahelper/GeneratorProperty.java
+++ szcshl-web/src/main/java/cs/ahelper/GeneratorProperty.java
@@ -11,24 +11,23 @@ public class GeneratorProperty {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("roleName:"+Role_.roleName.getName());
-	
-		
-		Class<?> dto=RoleDto.class;
-		
-		Method[] methods=dto.getMethods();
-		Set<String> methods2=new HashSet<>();
-		
-		for (Method m : methods) {			
-			if(m.getName().contains("get")||m.getName().contains("set")){
-				if(m.getName()!="getClass"){
+		System.out.println("roleName:" + Role_.roleName.getName());
+
+		Class<?> dto = RoleDto.class;
+
+		Method[] methods = dto.getMethods();
+		Set<String> methods2 = new HashSet<>();
+
+		for (Method m : methods) {
+			if (m.getName().contains("get") || m.getName().contains("set")) {
+				if (m.getName() != "getClass") {
 					methods2.add(m.getName().replace("get", "").replace("set", ""));
-				}					
+				}
 			}
-					
+
 		}
 		for (String string : methods2) {
-			System.out.println(String.format("roleDto.set%s(role.get%s());", string,string));
+			System.out.println(String.format("roleDto.set%s(role.get%s());", string, string));
 		}
 	}
 
