8171332c001cddac4875138871518388683c51e4
tzg <tang_guo_168@163.com>
2017-05-04 12:00:06 +0800

    调整用户模块的代码，处理一些小bug,以及去除target目录的忽略

--- szcshl-web/src/main/webapp/contents/app/user/user.edit.ctrl.js
+++ szcshl-web/src/main/webapp/contents/app/user/user.edit.ctrl.js
@@ -5,35 +5,35 @@
         .module('app')
         .controller('userEditCtrl', user);
 
-    user.$inject = ['$location','userSvc','$state']; 
+    user.$inject = ['$location', 'userSvc', '$state'];
 
-    function user($location, userSvc,$state) {
+    function user($location, userSvc, $state) {
         /* jshint validthis:true */
         var vm = this;
         vm.title = '添加用户';
-        vm.isuserExist=false;
+        vm.isuserExist = false;
         vm.id = $state.params.id;
         if (vm.id) {
             vm.isUpdate = true;
             vm.title = '更新用户';
         }
-        
+
         vm.create = function () {
-        	userSvc.createUser(vm);
+            userSvc.createUser(vm);
         };
         vm.update = function () {
-        	userSvc.updateUser(vm);
-        };      
+            userSvc.updateUser(vm);
+        };
 
         activate();
         function activate() {
-        	if (vm.isUpdate) {
-        		userSvc.getUserById(vm);
+            if (vm.isUpdate) {
+                userSvc.getUserById(vm);
             } else {
-            	userSvc.initZtreeClient(vm);
-            	userSvc.getDict(vm);
+                userSvc.initZtreeClient(vm);
+                userSvc.getDict(vm);
             }
-        	userSvc.getOrg(vm);
+            userSvc.getOrg(vm);
         }
     }
 })();
