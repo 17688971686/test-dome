d4b1de16da7fed398aef79afb4a6cc4f21f71137
tzg <tang_guo_168@163.com>
2017-05-08 15:31:39 +0800

    修改样例的controller

--- szcshl-web/src/main/java/cs/controller/demo/MyTestController.java
+++ szcshl-web/src/main/java/cs/controller/demo/MyTestController.java
@@ -16,34 +16,34 @@ import java.text.ParseException;
 import java.util.Date;
 
 /**
- * Description: my test controller
- * User: Administrator
- * Date: 2017/5/4 17:33
+ * Description: My test 控制层
+ * author: tzg
+ * Date: 2017-5-8 15:20:53
  */
 @Controller
-@RequestMapping(name = "My test", path = "mytest")
+@RequestMapping(name = "My test", path = "myTest")
 public class MyTestController {
 
     @Autowired
     private MyTestService myTestService;
 
-    @RequiresPermissions("mytest##get")
-    @RequestMapping(name = "获取My test数据", path = "", method = RequestMethod.GET)
+    @RequiresPermissions("myTest##get")
+    @RequestMapping(name = "获取数据", path = "", method = RequestMethod.GET)
     @ResponseBody
     public PageModelDto<MyTestDto> get(HttpServletRequest request) throws ParseException {
         ODataObj odataObj = new ODataObj(request);
         return myTestService.getDto(odataObj);
     }
 
-    @RequiresPermissions("mytest##post")
-    @RequestMapping(name = "创建My test", path = "", method = RequestMethod.POST)
+    @RequiresPermissions("myTest##post")
+    @RequestMapping(name = "创建记录", path = "", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public void post(@RequestBody MyTestDto record) {
         myTestService.create(record);
     }
 
-    @RequiresPermissions("mytest##delete")
-    @RequestMapping(name = "删除My test", path = "", method = RequestMethod.DELETE)
+    @RequiresPermissions("myTest##delete")
+    @RequestMapping(name = "删除记录", path = "", method = RequestMethod.DELETE)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void delete(@RequestBody String id) {
         String[] ids = id.split(",");
@@ -54,24 +54,24 @@ public class MyTestController {
         }
     }
 
-    @RequiresPermissions("mytest##put")
-    @RequestMapping(name = "更新My test", path = "", method = RequestMethod.PUT)
+    @RequiresPermissions("myTest##put")
+    @RequestMapping(name = "更新记录", path = "", method = RequestMethod.PUT)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void put(@RequestBody MyTestDto record) {
         myTestService.update(record);
     }
 
     // begin#html
-    @RequiresPermissions("mytest#html/list#get")
-    @RequestMapping(name = "用户列表页面", path = "html/list", method = RequestMethod.GET)
+    @RequiresPermissions("myTest#html/list#get")
+    @RequestMapping(name = "列表页面", path = "html/list", method = RequestMethod.GET)
     public String list() {
-        return "mytest/list";
+        return "myTest/myTestList";
     }
 
-    @RequiresPermissions("mytest#html/edit#get")
-    @RequestMapping(name = "编辑用户页面", path = "html/edit", method = RequestMethod.GET)
+    @RequiresPermissions("myTest#html/edit#get")
+    @RequestMapping(name = "编辑页面", path = "html/edit", method = RequestMethod.GET)
     public String edit() {
-        return "mytest/edit";
+        return "myTest/myTestEdit";
     }
     // end#html
 
