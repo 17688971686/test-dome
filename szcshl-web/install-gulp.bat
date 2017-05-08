f7b6507c3b7d3b4c24ccb4c93fee268cd85748f6
tzg <tang_guo_168@163.com>
2017-05-08 14:28:52 +0800

    添加gulp插件的安装命令

--- szcshl-web/install-gulp.bat
+++ szcshl-web/install-gulp.bat
@@ -1,4 +1,4 @@
-
+call npm install gulp --global --registry=https://registry.npm.taobao.org
 call npm install gulp-jshint gulp-concat gulp-uglify gulp-clean gulp-notify gulp-rename --save-dev --registry=https://registry.npm.taobao.org
 
 pause
