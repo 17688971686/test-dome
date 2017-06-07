9016e4662f1ec85aa9e8e90999b8caef9f8a7279
tzg <tang_guo_168@163.com>
2017-06-07 09:33:36 +0800

    增加样例模块的显示

--- szcshl-web/src/main/webapp/contents/app/demo/demo.ctrl.js
+++ szcshl-web/src/main/webapp/contents/app/demo/demo.ctrl.js
@@ -1,47 +1,43 @@
 (function () {
     'use strict';
 
-    angular
-        .module('app')
-        .controller('demoCtrl', demo);
+    angular.module('app').controller('demoCtrl', demo);
 
-    demo.$inject = ['$location','demoSvc']; 
+    demo.$inject = ['$location', 'demoSvc'];
 
     function demo($location, demoSvc) {
         /* jshint validthis:true */
-    	var vm = this;
+        var vm = this;
         vm.title = '';
-        
-        
-        vm.showDialog=function(){
-        	
-        	 $('.myDialog').modal({
-                 backdrop: 'static',
-                 keyboard:false
-             });
+
+        vm.showDialog = function () {
+
+            $('.myDialog').modal({
+                backdrop: 'static',
+                keyboard: false
+            });
         }
-        
-        function datetimePicker(){
-        	$("#datepicker").kendoDatePicker({
-        		culture:'zh-CN'
-        	});
+
+        // function datetimePicker() {
+            // $("#datepicker").kendoDatePicker();
+            // $("#datetiempicker").kendoDateTimePicker();
+        // }
+
+        function upload() {
+            $("#files").kendoUpload({
+                async: {
+                    saveUrl: rootPath + "/demo/save",
+                    removeUrl: rootPath + "/demo/remove",
+                    autoUpload: true
+                }
+            });
         }
-        
-        function upload(){
-        	 $("#files").kendoUpload({
-                 async: {
-                     saveUrl: "/demo/save",
-                     removeUrl: "/demo/remove",
-                     autoUpload: true
-                 }
-             });
-        }
-       
-       
+
+
         activate();
         function activate() {
-        	datetimePicker();
-        	upload();
+            // datetimePicker();
+            upload();
         }
     }
 })();
