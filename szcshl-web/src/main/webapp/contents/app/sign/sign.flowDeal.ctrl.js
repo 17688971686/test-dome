b6d3b28d91e6c255ed9af963993418d6c0d83527
cs\ldm <512887@qq.com>
2017-12-08 09:34:13 +0800

    工作方案模板打印

--- szcshl-web/src/main/webapp/contents/app/sign/sign.flowDeal.ctrl.js
+++ szcshl-web/src/main/webapp/contents/app/sign/sign.flowDeal.ctrl.js
@@ -970,13 +970,10 @@
             })
         }
 
-        //签收模板打印
+        //工作方案模板打印
         vm.printpage = function ($event) {
-          var id =  $($event.target).parent().attr("id");
-            console.log(id);
-            console.log("longyuyue");
-
-            //templatePrintSvc.templatePrint($event.target,vm.model);
+            var id =  $($event.target).attr("id");
+            signSvc.workProgramPrint(id);
         }
 
         /**
