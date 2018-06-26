03d89f84715881a6c9be4864f3e601c5f4cbc1b9
gs <zhaoyan_xi@sina.com>
2018-06-26 16:54:28 +0800

    本地存储短信、批复金额2

--- szcshl-web/src/main/java/cs/repository/repositoryImpl/project/SignRepoImpl.java
+++ szcshl-web/src/main/java/cs/repository/repositoryImpl/project/SignRepoImpl.java
@@ -185,8 +185,8 @@ public class SignRepoImpl extends AbstractRepository<Sign, String> implements Si
         criteria.add(Restrictions.ge(Sign_.processState.getName(), Constant.SignProcessState.END_DIS_NUM.getValue()));
 
         List<Sign> signList = criteria.list();
-        //过滤掉手工签收的项目，即收文编号为0000结尾的项目 .filter(s -> (!s.getFilecode().endsWith("0000")))
-        List<Sign> resultList = signList.stream().collect(Collectors.toList());
+        //过滤掉手工签收的项目，即收文编号为0000结尾的项目
+        List<Sign> resultList = signList.stream().filter(s -> (!s.getFilecode().endsWith("0000"))).collect(Collectors.toList());
         return signList;
     }
 
