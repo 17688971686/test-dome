9e2ea25b5a236d174c8b0bf5e45514e2fda61d04
cs\ldm <512887@qq.com>
2018-12-05 18:23:18 +0800

    全角转换

--- szcshl-web/src/main/java/cs/xss/XssShieldUtil.java
+++ szcshl-web/src/main/java/cs/xss/XssShieldUtil.java
@@ -56,9 +56,54 @@ public class XssShieldUtil {
                     value = matcher.replaceAll("");
                 }
             }
-            value = value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+            value = xssEncode(value);
         }
         return value;
     }
 
+    //将容易引起xss漏洞的半角字符直接替换成全角字符
+    public static String xssEncode(String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder(s.length() + 16);
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+
+                case '>':
+                    sb.append('＞');          //全角大于号
+                    break;
+
+                case '<':
+                    sb.append('＜');          //全角小于号
+                    break;
+
+                case '\'':
+                    sb.append('‘');           //全角单引号
+                    break;
+
+                case '\"':
+                    sb.append('“');           //全角双引号
+                    break;
+
+                case '&':
+                    sb.append('＆');          //全角
+                    break;
+
+                case '\\':
+                    sb.append('＼');          //全角斜线
+                    break;
+
+                case '#':
+                    sb.append('＃');          //全角井号
+                    break;
+
+                default:
+                    sb.append(c);
+                    break;
+            }
+        }
+        return sb.toString();
+    }
 }
